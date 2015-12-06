cac21237d9c03a877a2cac4f77634c6ead508987
AlexMtz <yamop.16@gmail.com>
2015-12-06 10:09:07 -0600

    Se añadieron las primeras pruebas unitTest para las compras

--- src/compras/tests.py
+++ src/compras/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from compras.views import listar, nueva_compra, eliminar_compra
+from django.http import HttpRequest
 
-# Create your tests here.
+
+class ComprasTest(TestCase):
+
+    def probar_la_url_de_listar_las_compras(self):
+        found = resolve('/')
+        self.assertEqual(found.func, listar)
+
+    def probar_la_url_de_crear_una_nueva_compra(self):
+        found = resolve('/nueva_compra')
+        self.assertEqual(found.func, nueva_compra)
+
+    def probar_la_url_de_eliminar_una_compra(self, id_compra_eliminar):
+        found = resolve('/' + id_compra_eliminar + '/eliminar')
+        self.assertEqual(found.func, eliminar_compra)
+
+    def probar_si_listar_devuelve_los_datos_esperados(self):
+        request = HttpRequest()
+        response = listar(request)
+        self.assertTrue(response.content.endswith('</html>'))
