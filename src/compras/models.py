3dd41ba9f0aa8d0b29d64c9aa263b5800025ef67
AlexMtz <yamop.16@gmail.com>
2015-12-06 12:49:49 -0600

    Se modifico el archivo de lista.html y models.py de las compras para mostrar el historial de las compras

--- src/compras/models.py
+++ src/compras/models.py
@@ -1,7 +1,9 @@
+# coding: utf-8 #
 from django.db import models
 from proveedores.models import Proveedor
 from empleados.models import Empleado
 from inventarios.models import Inventario
+
 
 class Compra(models.Model):
     id_proveedor = models.ForeignKey(Proveedor)
@@ -11,4 +13,4 @@ class Compra(models.Model):
     monto = models.IntegerField()
 
     def __unicode__(self):
-        return "Compra: " + self.id + " fecha: " + self.fecha
+        return u"Número de compra: [" + str(self.id) + "] realizada el: " + str(self.fecha)
