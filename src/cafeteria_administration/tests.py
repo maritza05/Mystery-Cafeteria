a68e89f62aff503ed8085c4b8d6cc39b9b91b88b
Maritza <maritzaesparza05@gmail.com>
2015-12-05 23:24:17 -0600

    Refactorización de la prueba con autopep8

--- src/cafeteria_administration/tests.py
+++ src/cafeteria_administration/tests.py
@@ -2,16 +2,17 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from cafeteria_administration.views import cafeteria
+from django.template.loader import render_to_string
+
 
 class HomePageTest(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/cafeteria/')
-		self.assertEqual(found.func, cafeteria)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/cafeteria/')
+        self.assertEqual(found.func, cafeteria)
 
-	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = cafeteria(request)
-		self.assertTrue(response.content.startswith(b'<html>'))
-		self.assertIn(b'<title>Mystery-Cafeteria</title>', response.content)
-		self.assertTrue(response.content.endswith(b'</html>'))
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = cafeteria(request)
+        expected_html = render_to_string('cafeteria.html')
+        self.assertEqual(response.content.decode(), expected_html)
