d4b2db5c091604172705ed06a4f54f0e4b2bc50f
Maritza <maritzaesparza05@gmail.com>
2015-12-05 22:22:59 -0600

    Añadidas las URLS de las páginas principales

--- src/cafeteria/urls.py
+++ src/cafeteria/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+
 urlpatterns = [
     # Examples:
-    url(r'^$', 'cafeteria.views.home', name='home'),
+    # url(r'^$', 'cafeteria.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'cafeteria_administration.views.index', name='index'),
+    url(r'cafeteria/', 'cafeteria_administration.views.cafeteria', name='cafeteria'),
     url(r'^admin/', include(admin.site.urls)),
 ]
