d2f9cca04b91573e1800874ae8591f1920f91cb6
Maritza <maritzaesparza05@gmail.com>
2015-12-05 22:15:06 -0600

    Añadida la prueba para la página principal

--- src/cafeteria_administration/tests.py
+++ src/cafeteria_administration/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+from cafeteria_administration.views import cafeteria
 
-# Create your tests here.
+class HomePageTest(TestCase):
+
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/cafeteria')
+		self.assertEqual(found.func, cafeteria)
+
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = cafeteria(request)
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertIn(b'<title>Mystery-Cafeteria</title>', response.content)
+		self.assertTrue(response.content.endswith(b'</html>'))
