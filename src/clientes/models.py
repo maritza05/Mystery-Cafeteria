363140d5e9c2bfc09293e9ed15d066c11b04664a
VaneAR <vdrar.06@gmail.com>
2015-12-07 13:45:20 -0600

    Campos acomodados

--- src/clientes/models.py
+++ src/clientes/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Cliente(models.Model):
-    password = models.CharField(max_length = 25)
     nombre = models.CharField(max_length = 65)
+    password = models.CharField(max_length = 25)
     apellido_paterno = models.CharField('Apellido Paterno', max_length = 120)
     apellido_materno = models.CharField('Apellido Materno', max_length = 120)
     mail = models.EmailField('E-mail', max_length = 60)
