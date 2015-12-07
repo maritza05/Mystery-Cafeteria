30acf01ce16ff85ab2237daaf7c95e9e32c9bd30
VaneAR <vdrar.06@gmail.com>
2015-12-07 13:46:19 -0600

    Añadida url para consultas

--- src/clientes/urls.py
+++ src/clientes/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from clientes.views import listar, nuevo_cliente, eliminar_cliente, modificar_clientes
+from clientes.views import listar, nuevo_cliente, eliminar_cliente, modificar_cliente, consultar_cliente
+
 
 urlpatterns = [
     url(r'^$', listar, name="listar"),
-    url(r'^nuevo/$', nuevo_cliente, name="nueva_cliente"),
-    url(r'^(?P<pk>[0-9]+)/modificar', modificar_clientes, name="modificar_clientes "),
-    url(r'^(?P<pk>[0-9]+)/eliminar', eliminar_compra, name="eliminar_cliente"),
+    url(r'^nuevo/$', nuevo_cliente, name="nuevo_cliente"),
+    url(r'^(?P<pk>[0-9]+)/modificar', modificar_cliente, name="modificar_cliente"),
+    url(r'^(?P<pk>[0-9]+)/eliminar', eliminar_cliente, name="eliminar_cliente"),
+     url(r'^(?P<pk>[0-9]+)/consultar', consultar_cliente, name="consultar_cliente"),
 ]
