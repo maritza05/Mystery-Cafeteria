99f3ba20ce199c5937661fd94d884ef06f8f9e22
VaneAR <vdrar.06@gmail.com>
2015-12-07 20:59:48 -0600

    Pruebas unitarias listas

--- src/clientes/tests.py
+++ src/clientes/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
-from proveedores.views import listar, nuevo_cliente, eliminar_cliente, editar_cliente
+from clientes.views import listar, nuevo_cliente, eliminar_cliente, modificar_cliente, consulta_cliente
 from django.http import HttpRequest
 
 
@@ -12,18 +12,29 @@ class ClientesTest(TestCase):
 
     def test_titulo_de_la_lista_clientes(self):
         request = HttpRequest()
-        response = nuevo_proveedor(request)
+        response = nuevo_cliente(request)
         self.assertTrue(response.content.contains('Lista Clientes'))
 
     def test_url_agregar_nuevo_cliente(self):
-        found = resolve('/nuevo_cliente')
+        found = resolve('/nuevo')
         self.assertEqual(found.func, nuevo_cliente)
 
+    def test_titulo_de_agregar_clientes(self):
+        request = HttpRequest()
+        response = nuevo_cliente(request)
+        self.assertTrue(response.content.contains('Agregar Cliente'))
+
+
     def test_url_editar_cliente_(self, id_editar_cliente):
-        found = resolve('/' + id_editar_cliente + '/editar_cliente')
+        found = resolve('/' + id_editar_cliente + '/modificar')
         self.assertEqual(found.func, editar_cliente)
+
+    def test_titulo_de_modificar_clientes(self):
+        request = HttpRequest()
+        response = nuevo_cliente(request)
+        self.assertTrue(response.content.contains('Modificar Cliente'))
 
 
     def test_url_eliminar_cliente(self, id_eliminar_cliente):
-        found = resolve('/' + id_eliminar_cliente + '/eliminar_cliente')
+        found = resolve('/' + id_eliminar_cliente + '/eliminar')
         self.assertEqual(found.func, eliminar_cliente)
