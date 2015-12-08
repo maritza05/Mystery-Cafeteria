567f57d8569f24c8ac0d9c0b5c736393f86a6d03
RamRaidan <dr.raigoza@gmail.com>
2015-12-08 00:08:10 -0600

    Se añadio vista listar, y modificar inventario

--- src/inventarios/views.py
+++ src/inventarios/views.py
@@ -5,4 +5,16 @@ from inventarios.models import Inventario
 def listar(request):
     inventarios = Inventario.objects.all()
     return render(request, 'inventarios/lista.html', {'inventarios': inventarios})
+
+def modificar_inventario(request, pk):
+    inventarios = get_object_or_404(Inventario, pk=pk)
+    if request.method == "POST":
+        form = PuestoForm(request.POST, instance=puestos)
+        if form.is_valid():
+            puestos = form.save()
+            puestos.save()
+            return redirect('listar')
+    else:
+        form = PuestoForm(instance=puestos)
+    return render(request, 'puestos/nuevo_puesto.html', {'form':form, 'etiqueta_titulo': 'Modificar Puesto', 'etiqueta_boton':'Actualizar'})
 # Create your views here.
